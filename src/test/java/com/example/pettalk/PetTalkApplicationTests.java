00944074857f7505ef139e6b9359f26655c2e31a
Lim Suyoung <leepari20@gmail.com>
2023-06-30 14:22:33 +0900

    init

---
+++ src/test/java/com/example/pettalk/PetTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pettalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
