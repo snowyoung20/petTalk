1f2acce25272923139317f092746e96d2cf835c8
Lim Suyoung <leepari20@gmail.com>
2023-07-03 21:50:57 +0900

    test

--- src/main/java/com/example/pettalk/config/WebSecurityConfig.java
+++ src/main/java/com/example/pettalk/config/WebSecurityConfig.java
@@ -1,4 +1,12 @@
 package com.example.pettalk.config;
 
+import com.example.pettalk.service.UserDetailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
 public class WebSecurityConfig {
+	private final UserDetailService userService;
+
 }
---
+++ src/main/java/com/example/pettalk/service/UserDetailService.java
@@ -0,0 +1,22 @@
+package com.example.pettalk.service;
+
+
+import com.example.pettalk.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailService implements UserDetailsService {
+
+	private final UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String userId) {
+		return userRepository.findByUserId(userId)
+				.orElseThrow(() -> new IllegalArgumentException((userId)));
+	}
+}
